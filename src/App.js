fc373337d7769f62bbdf9be3f4ca4b403d461d08
Dodger-42 <kevin.bing.barber@gmail.com>
2020-09-03 15:33:15 -0700

    making it happen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
@@ -11,56 +11,50 @@ import "./styles.css";
 import usefetchShow from "../api/usefetchShow.js";
 
 export default function App() {
- // const [show, setShow] = useState(null);
- // const [seasons, setSeasons] = useState([]);
- // const [selectedSeason, setSelectedSeason] = useState("");
- // const episodes = seasons[selectedSeason] || [];
+ const [show, setShow] = useState(null);
+ const [seasons, setSeasons] = useState([]);
+ const [selectedSeason, setSelectedSeason] = useState("");
+ const episodes = seasons[selectedSeason] || [];
 
 
- const intialValues = {
-   show: null,
-   seasons: "",
-   selectedSeason: "",
-   episodes: [seasons[selectedSeason]] || []
- };
+//  const intialValues = {
+//    show: null,
+//    seasons: "",
+//    selectedSeason: "",
+//    episodes: [seasons[selectedSeason]] || []
+//  };
 
 
- const [  show, seasons, handleSelect, selectedSeason, episodes] = usefetchShow(intialValues);
-}
-//   useEffect(() => {
-//     const fetchShow = () => {
-//       axios
-//         .get(
-//           "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-//         )
-//         .then(res => {
-//           setShow(res.data);
-//           setSeasons(formatSeasons(res.data._embedded.episodes));
-//         });
-//     };
-//     fetchShow();
-//   }, []);
-
-//   // const handleSelect = e => {
-//   //   setSelectedSeason(e.value);
-//   // };
-
-//   if (!show) {
-//     return <h2>Fetching data...</h2>;
-//   }
-
-//   return (
-//     <div className="App">
-//       <img className="poster-img" src={show.image.original} alt={show.name} />
-//       <h1>{show.name}</h1>
-//       {parse(show.summary)}
-//       <Dropdown
-//         options={Object.keys(seasons)}
-//         onChange={handleSelect}
-//         value={selectedSeason || "Select a season"}
-//         placeholder="Select an option"
-//       />
-//       <Episodes episodes={episodes} />
-//     </div>
-//   );
+//  const [  show, seasons, handleSelect, selectedSeason, episodes] = usefetchShow(intialValues);
 // }
+  useEffect(() => {
+    fetchShow()    
+        .then(res => {
+          setShow(res.data);
+          setSeasons(formatSeasons(res.data._embedded.episodes));
+        });
+  }, []);
+
+  const handleSelect = e => {
+    setSelectedSeason(e.value);
+  };
+
+  if (!show) {
+    return <h2>Fetching data...</h2>;
+  }
+
+  return (
+    <div className="App">
+      <img className="poster-img" src={show.image.original} alt={show.name} />
+      <h1>{show.name}</h1>
+      {parse(show.summary)}
+      <Dropdown
+        options={Object.keys(seasons)}
+        onChange={handleSelect}
+        value={selectedSeason || "Select a season"}
+        placeholder="Select an option"
+      />
+      <Episodes episodes={episodes} />
+    </div>
+  );
+}
--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -3,10 +3,23 @@ import Episodes from './Episodes';
 import {render} from '@testing-library/react'
 
 
-test('Component Renders', () => {
-    render(<Episodes />)
-  })
+// test('Component Renders', () => {
+//     render(<Episodes />)
+//   });
 
 test('fake test', () => {
     expect(true).toBeTruthy()
 });
+
+test("Episodes component renders", ()=>{
+    const {queryAllByTestId, rerender} = render(<Episodes episodes={[]}/>);
+
+    expect(queryAllByTestId("episode").toHaveLength(1));
+});
+
+const data = [
+    {
+        id: '1',
+        name: "Episode 1"
+    }
+];
